8b9f2a54a659f7567d2c1c1ca18975094266ed31
manu1519 <menox_2@hotmail.com>
2026-04-16 01:25:41 -0600

    This is the update of a full configuration of the Clocks, using HSE

--- Scripts/CLOCKS/ManuFiles/main.c
+++ Scripts/CLOCKS/ManuFiles/main.c
@@ -2,12 +2,15 @@
 
 #include "stm32f446xx.h"
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_cortex.h"
 #include "stm32f4xx_hal_def.h"
+#include "stm32f4xx_hal_flash_ex.h"
 #include "stm32f4xx_hal_rcc.h"
 #include "stm32f4xx_hal_uart.h"
 #include "main.h"
 #include "string.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <sys/types.h>
 
 #define TRUE 1
@@ -23,6 +26,7 @@ int main(void){
 
     RCC_OscInitTypeDef osc_init;
     RCC_ClkInitTypeDef clk_init;
+    char msg[100];
     HAL_Init();
     UART2_Init();
 
@@ -33,7 +37,42 @@ int main(void){
     if (HAL_RCC_OscConfig(&osc_init) != HAL_OK) {
         Error_handler();
     }
+
+    // Initializing the Variable which contains the Clock Configurations
+    clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+    clk_init.AHBCLKDivider = RCC_SYSCLK_DIV2;
+    clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+    clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if(HAL_RCC_ClockConfig(&clk_init, FLASH_ACR_LATENCY_0WS) != HAL_OK)
+        {
+            Error_handler();
+        }
+
+    __HAL_RCC_HSI_ENABLE();   // Saves some current
+
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    UART2_Init();
+
+    memset(msg, 0, sizeof(msg));
+    sprintf(msg, "SYSCLK : %ld\r\n", HAL_RCC_GetSysClockFreq());
+    HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+    memset(msg, 0, sizeof(msg));
+    sprintf(msg, "HCLK : %ld\r\n", HAL_RCC_GetHCLKFreq());
+    HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+    memset(msg, 0, sizeof(msg));
+    sprintf(msg, "PCLK1 : %ld\r\n", HAL_RCC_GetPCLK1Freq());
+    HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
     
+    memset(msg, 0, sizeof(msg));
+    sprintf(msg, "PCLK2 : %ld\r\n", HAL_RCC_GetPCLK2Freq());
+    HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);    
 
     while (1);
 
